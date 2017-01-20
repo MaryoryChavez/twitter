6ddb444a72c9a83742ce5a863a54a4660a3892fe
MaryoryChavez <maryory.chavez@gmail.com>
2017-01-20 18:09:31 -0500

    funcion eliminar

--- JS/main.js
+++ JS/main.js
@@ -1,27 +1,46 @@
+    var lista = document.getElementById('add-list');
+    var listatrash = document.getElementById('trash');
+
 function update(){
     var text = document.getElementById("area").value;
-    add(text);   
+    add(text, lista, listatrash);   
 }
 
-function add(_text){
-    var lista = document.getElementById('add-list');
-    var div = document.createElement('div');
-    div.innerHTML = _text;
-    //div.className = "col-sm-6 col-xs-6";
-    lista.appendChild(div);
+function add(_text, _lista, _listatrash){
     
-    var listatrash = document.getElementById('trash');
+    var div = document.createElement('div');
+    div.innerHTML = '<input type="checkbox">'+_text;
+    //div.className = "col-sm-6 col-xs-6";
+    _lista.appendChild(div);
+    
     var div1 = document.createElement('div');
-    div1.innerHTML = "trash";
+    
+    var obj = {
+        tagain : '<a href="" >',
+        tagaend : '</a>',
+        trashi : '<i class="fa fa-trash-o" aria-hidden="true"></i>',
+    };
+    div1.innerHTML = obj.tagain + obj.trashi + obj.tagaend + _text;
+    //div1.innerHTML = '<a href="" onclick="trash(); return false;"><i class="fa fa-trash-o" aria-hidden="true"></i></a>';
     //div1.className = "col-sm-6 col-xs-6 text-right";
-    listatrash.appendChild(div1);
-
+    _listatrash.appendChild(div1);
+    
+    div1.onclick = function(){
+        this.parentNode.removeChild(this); 
+        div.parentNode.removeChild(div);
+        return false;
+    }
 }
 
 function cross(){
-    
+       
 }
 
-function trash(){
-    
-}
+
+
+/*function trash(_div,_div1){
+   var a=document.getElementsByTagName("A");
+    _div.parentNode.removeChild(_div);
+    _div1.parentNode.removeChild(_div1);
+    return false;
+}*/
