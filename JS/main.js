5e48d08b3e23a20de3a669564353887f73a208c8
MaryoryChavez <maryory.chavez@gmail.com>
2017-01-23 18:35:33 -0500

    desarrollo v2

--- JS/main.js
+++ JS/main.js
@@ -1,46 +1,89 @@
-    var lista = document.getElementById('add-list');
-    var listatrash = document.getElementById('trash');
+var btn = document.getElementById("btn");
 
-function update(){
+btn.onclick = function verificar(area){
     var text = document.getElementById("area").value;
-    add(text, lista, listatrash);   
-}
-
-function add(_text, _lista, _listatrash){
-    
-    var div = document.createElement('div');
-    div.innerHTML = '<input type="checkbox">'+_text;
-    //div.className = "col-sm-6 col-xs-6";
-    _lista.appendChild(div);
-    
-    var div1 = document.createElement('div');
-    
-    var obj = {
-        tagain : '<a href="" >',
-        tagaend : '</a>',
-        trashi : '<i class="fa fa-trash-o" aria-hidden="true"></i>',
-    };
-    div1.innerHTML = obj.tagain + obj.trashi + obj.tagaend + _text;
-    //div1.innerHTML = '<a href="" onclick="trash(); return false;"><i class="fa fa-trash-o" aria-hidden="true"></i></a>';
-    //div1.className = "col-sm-6 col-xs-6 text-right";
-    _listatrash.appendChild(div1);
-    
-    div1.onclick = function(){
-        this.parentNode.removeChild(this); 
-        div.parentNode.removeChild(div);
-        return false;
+        if(area.value!=""){
+            return btn.disabled = false;
+            alert(text);
+        }else{
+            return btn.disabled = true;
+            alert(text);
+        }
     }
+
+function erased(){
+    
 }
 
 function cross(){
-       
+    
 }
 
 
 
-/*function trash(_div,_div1){
-   var a=document.getElementsByTagName("A");
-    _div.parentNode.removeChild(_div);
-    _div1.parentNode.removeChild(_div1);
-    return false;
-}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    //version 1.0
+    
+    var lista = document.getElementById('add-list');
+    var listatrash = document.getElementById('trash');
+
+    function update() {
+        var text = document.getElementById("area").value;
+        var btn = document.getElementById("btn");
+        var yes = function () {
+            btn.disabled = true;
+        }
+        var no = function () {
+            btn.disabled = false;
+        }
+        if (text.length == 0) {
+            return yes;
+        } else {
+            add(text, lista, listatrash);
+            //lista.addEventListener("change", cross())
+            return no;
+        }
+    }
+
+    function add(_text, _lista, _listatrash) {
+        var div = document.createElement('div');
+        div.innerHTML = '<input type="checkbox">' + _text;
+        _lista.appendChild(div);
+        var div1 = document.createElement('div');
+        var obj = {
+            tagain: '<a href="" >',
+            tagaend: '</a>',
+            trashi: '<i class="fa fa-trash-o" aria-hidden="true"></i>',
+        };
+        div1.innerHTML = obj.tagain + obj.trashi + obj.tagaend;
+        _listatrash.appendChild(div1);
+
+        div1.onclick = function () {
+            this.parentNode.removeChild(this);
+            div.parentNode.removeChild(div);
+            return false;
+        }
+    }
+    function cross(div) {
+       var check=div.target.parentNode;
+	if(div.target.checked)
+		check.setAttribute("style","text-decoration:line-through");
+	else
+		check.setAttribute("style","text-decoration:none");
+    }
+*/
