c1a69e3d7bbdb470984b2ff73bd6e2c0eb99187c
MaryoryChavez <maryory.chavez@gmail.com>
2017-02-03 15:07:57 -0500

    version 2.0

--- JS/main.js
+++ JS/main.js
@@ -1,124 +1,69 @@
+//version 2.0
+var listaTarea = [];
 var btn = document.getElementById("btn");
-var text = document.getElementById("area");
-text.onkeyup = function () {
-    if (text.value.length > 0) {
+var area = document.getElementById("area");
+
+function blocke() {
+    if (area.value.length > 0) {
         btn.disabled = false;
     } else {
         btn.disabled = true;
     }
-    btn.addEventListener("click", add);
 }
-function add() {
-    var lista = document.getElementById('add-list');
-    var listatrash = document.getElementById('trash');
-    var div = document.createElement("div");
-    lista.appendChild(div);
-    var checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    var span = document.createElement("span");
-    var work = document.createTextNode(text.value);
-    span.appendChild(work);
-    div.appendChild(checkbox);
-    div.appendChild(span);
-    var div1 = document.createElement('div');
-    var obj = {
-        tagain: '<a href="#" class="list-group-item">',
-        tagaend: '</a>',
-        trashi: '<i class="fa fa-trash-o" aria-hidden="true"></i>',
+
+function addWork() {
+    var listaText = document.getElementById("text");
+
+    var work = {
+        nombre: '<span>' + area.value + '</span>',
+        checkbox: '<input type="checkbox" class="inputcheck" onclick="cross(this)">',
+        checkcircle: '<i class="fa fa-check-circle" aria-hidden="true"></i>',
+        iconTrash: '<a href="#" onclick="erasedList(this)"><i class="fa fa-trash-o" aria-hidden="true"></i></a>',
     };
-    div1.innerHTML = obj.tagain + obj.trashi + obj.tagaend;
-    listatrash.appendChild(div1);
-
-    div1.onclick = function () {
-        this.parentNode.removeChild(this);
-        div.parentNode.removeChild(div);
-        return false;
+    var html;
+    listaTarea.push(work);
+    for (var i in listaTarea) {
+        html = '<p>' + '<label>' + listaTarea[i].checkbox + listaTarea[i].checkcircle + '</label>' + listaTarea[i].nombre + listaTarea[i].iconTrash + '</p>';
     }
-    if (lista.childElementCount > 0) {
-        checkbox.addEventListener("click", cross);
-    }
-}
-function cross(evt) {
-    var checkbox = evt.target;
-    var span = evt.target.parentNode.lastChild;
-    var div = evt.target.parentNode;
-    
-    //console.log(evt.target.parentNode);
-    if (checkbox.checked) {
-        span.classList.add("cross");
-        div.classList.add("backgr");
-        
-    } else {
-        span.classList.remove("cross");
-        div.classList.remove("backgr");
-    }
+    listaText.innerHTML += html;
+    area.value = '';
+    area.focus();
+    blocke();
 }
 
+function cross(_checkbox) {
+    var text = _checkbox.parentNode.nextSibling;
+    var conten = _checkbox.parentNode.parentNode;
+    var trash = _checkbox.parentNode.nextSibling.nextSibling;
+    var checkCircle = _checkbox.nextSibling;
+    _checkbox.checked ? (text.classList.add("cross"), conten.classList.add("backgr"), trash.classList.add("trash"), checkCircle.classList.add("check")) : (text.classList.remove("cross"), conten.classList.remove("backgr"), trash.classList.remove("trash"), checkCircle.classList.remove("check"));
+}
 
+function erasedList(_buttonTrash) {
+    var p = _buttonTrash.parentNode.parentNode;
+    p.removeChild(_buttonTrash.parentNode);
+}
 
+/*
+var listaTarea = [
+    {
+        nombre: "tarea",
+        isDone: true
+        },
+    {
+        nombre: "t2",
+        isDone: false
+        },
+];
 
-
-
-
-
-
-
-
-
-
-
-
-
+*/
 
 
 /*
-    //version 1.0
+var lista = document.getElementById("add-list");
     
-    var lista = document.getElementById('add-list');
-    var listatrash = document.getElementById('trash');
-
-    function update() {
-        var text = document.getElementById("area").value;
-        var btn = document.getElementById("btn");
-        var yes = function () {
-            btn.disabled = true;
-        }
-        var no = function () {
-            btn.disabled = false;
-        }
-        if (text.length == 0) {
-            return yes;
-        } else {
-            add(text, lista, listatrash);
-            //lista.addEventListener("change", cross())
-            return no;
-        }
-    }
-
-    function add(_text, _lista, _listatrash) {
-        var div = document.createElement('div');
-        div.innerHTML = '<input type="checkbox">' + _text;
-        _lista.appendChild(div);
-        var div1 = document.createElement('div');
-        var obj = {
-            tagain: '<a href="" >',
-            tagaend: '</a>',
-            trashi: '<i class="fa fa-trash-o" aria-hidden="true"></i>',
-        };
-        div1.innerHTML = obj.tagain + obj.trashi + obj.tagaend;
-        _listatrash.appendChild(div1);
-
-        div1.onclick = function () {
-            this.parentNode.removeChild(this);
-            div.parentNode.removeChild(div);
-            return false;
-        }
-    }
-    function cross(div) {
-       var check=div.target.parentNode;
-	if(div.target.checked)
-		check.setAttribute("style","text-decoration:line-through");
-	else
-		check.setAttribute("style","text-decoration:none");
+    for (var i in listaTarea) {
+        var html = "<li><input type='checkbox' " + (listaTarea[i].isDone ? "checked" : "") + ">" + listaTarea[i].nombre + "</li>";
+        lista.innerHTML += html;
     }
 */
